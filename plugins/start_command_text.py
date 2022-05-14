b3e59e3d44f6a11b374ccf03f6548da65b0f87bc
InfoTech <infotechtele@gmail.com>
2022-05-14 22:48:08 +0530

    Update start_command_text.py

--- plugins/start_command_text.py
+++ plugins/start_command_text.py
@@ -25,10 +25,10 @@ bot_start_time = time.time()
 async def start(client, message):
     bot_uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time)) 
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("JOIN", url="https://t.me/TGBotsCollection")],
+        [InlineKeyboardButton("JOIN", url="https://t.me/infotechIT")],
         [InlineKeyboardButton(
-            "Try", url="https://t.me/TGBotsCollectionbot")]
+            "Try", url="https://t.me/AioMpSBot")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is Multipurpose Bot that can perform many functions.\n\n/help for More info \n Bot Uptime : {bot_uptime}"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is Aio Multipurpose Bot that can perform many functions.\n\n/help for More info \n Bot Uptime : {bot_uptime}"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
